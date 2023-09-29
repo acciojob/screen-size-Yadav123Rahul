5c66c8efb8c91dca8fbaa4356d200cf6f9febfeb
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-09-29 17:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function getSize(){
+    var width = document.documentElement.clientWidth;
+    var height = document.documentElement.clientHeight;
+    var output = document.getElementById("sizeInfo");
+    output.innerHTML = "<h1>Width: " + width + " and Height: " + height + "</h1>";
+}
